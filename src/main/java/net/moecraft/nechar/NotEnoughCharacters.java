eb1084bb7ed8bb7891ea456197bf04a8e3d6a714
vfyjxf <2331007009@qq.com>
2021-08-12 00:33:12 +0800

    增加对新元素的拼音支持

--- src/main/java/net/moecraft/nechar/NotEnoughCharacters.java
+++ src/main/java/net/moecraft/nechar/NotEnoughCharacters.java
@@ -2,6 +2,7 @@ package net.moecraft.nechar;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import me.towdium.pinin.DictLoader;
 import me.towdium.pinin.PinIn;
 import net.minecraft.client.Minecraft;
 import net.vfyjxf.nechar.NechConfig;
@@ -9,6 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.util.function.BiConsumer;
 
 
 @Mod(modid = NotEnoughCharacters.ID, useMetadata = true)
@@ -17,8 +19,28 @@ public class NotEnoughCharacters {
     public static final String VERSION = NotEnoughCharacters.class.getPackage().getImplementationVersion();
     public static final Logger logger = LogManager.getLogger("NotEnoughCharacters");
 
-    public static final PinIn context = new PinIn();
+    public static final PinIn context = new PinIn(new CustomDictLoader());
 
+    private static class CustomDictLoader extends DictLoader.Default {
+        @Override
+        public void load(BiConsumer<Character, String[]> feed) {
+            super.load(feed);
+            feed.accept('\ue900', new String[]{"lu2", "jinlu"}); // 钅卢
+            feed.accept('\ue901', new String[]{"du4", "jindu"}); //钅杜
+            feed.accept('\ue902', new String[]{"xi3", "jinxi"}); //钅喜
+            feed.accept('\ue903', new String[]{"bo1", "jinbo"}); //钅波
+            feed.accept('\ue904', new String[]{"hei1", "jinhei"}); //钅黑
+            feed.accept('\u9fcf', new String[]{"mai4", "jinmai"});//钅麦
+            feed.accept('\ue906', new String[]{"da2", "jinda"});//钅达
+            feed.accept('\ue907', new String[]{"lun2", "jinlun"});//钅仑
+            feed.accept('\u9fd4', new String[]{"ge1", "jinge"});//钅哥
+            feed.accept('\u9fed', new String[]{"ni3", "jiner"});//钅尔
+            feed.accept('\ue90a', new String[]{"fu1", "jinfu"});//钅夫
+            feed.accept('\ue90c', new String[]{"li4", "jinli"});//钅立
+            feed.accept('\u9fec', new String[]{"tian2", "shitian"});// 石田
+            feed.accept('\u9feb', new String[]{"ao4", "qiao", "aoqi"});//气奥
+        }
+    }
 
     private static void onConfigChange() {
         context.config()
@@ -30,6 +52,7 @@ public class NotEnoughCharacters {
                 .fEng2En(NechConfig.EnableFEng2En)
                 .fU2V(NechConfig.EnableFU2V).commit();
     }
+
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
